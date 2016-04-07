c429e70547807785fe1f0690ad624c64c392f423
afia <hussain_afia@hotmail.co.uk>
2016-04-07 13:17:59 +0100

    created a delete user page

--- application/views/pages/deleteUser.php
+++ application/views/pages/deleteUser.php
@@ -6,7 +6,7 @@ foreach($users as $user) {
 	echo $user->id . " ";
 	echo $user->firstname;
 	echo $user->lastname . " ";
-	echo "<a href=/index.php/pages/deleteUser.php/".$user->id.">delete</a> <br>";
+	echo "<a href=/index.php/pages/deleteUser/".$user->id.">delete</a> <br>";
 
 }
 
