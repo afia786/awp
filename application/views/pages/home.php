2eb67266acdfde3cb8c48255ae3848c68ad984e4
afia <hussain_afia@hotmail.co.uk>
2016-04-07 12:59:34 +0100

    changed the title

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,11 +1,12 @@
-<h1>Home1</h1>
+<h1>View (Results)</h1>
 
 <?php
 
 foreach($users as $user) {
 	echo $user->id . " ";
 	echo $user->firstname;
-	echo $user->lastname . "</br>";
+	echo $user->lastname . " ";
+	echo "<a href=/index.php/pages/updateUserForm/".$user->id.">Edit</a> <br>";
 
 }
 
